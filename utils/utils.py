af98272acbc6b251018cee23a0ebeed6ea166266
amiralserge <kossononserge@yahoo.fr>
2024-08-16 14:47:24 -0400

    extract open_image, resize_image, b64encode_image from local_image_to_data_url

--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,30 @@
 import base64
 from io import BytesIO
 from mimetypes import guess_type
-from typing import Iterator
+from typing import Iterator, Tuple
 
 import numpy as np
 from PIL import Image as ImageModule
+from PIL.Image import Image
 
 IMG_SIZE = (640, 427)
 
 
-def local_image_to_data_url(image_path) -> str:
+def open_image(image_path) -> Image:
+    return ImageModule.open(image_path)
+
+
+def resize_image(image: Image, size=IMG_SIZE) -> Image:
+    return image.resize(size)
+
+
+def b64encode_image(image: Image, format: str) -> str:
+    buffered = BytesIO()
+    image.save(buffered, format=format)
+    return base64.b64encode(buffered.getvalue()).decode()
+
+
+def local_image_to_data_url(image_path, size: Tuple = IMG_SIZE) -> str:
     """
     Function to encode a local image into data URL
     """
@@ -17,13 +32,13 @@ def local_image_to_data_url(image_path) -> str:
     if mime_type is None:
         # mime_type = 'image/png'
         raise Exception(f"Could not detect mime type of file `{image_path}`")
+    format = mime_type.split("/")[-1]
 
-    img = ImageModule.open(image_path).resize(IMG_SIZE)
-    _buffer = BytesIO()
-    img.save(_buffer, format=mime_type.split("/")[1])
-    base64_encoded_data = base64.b64encode(_buffer.getvalue()).decode("utf-8")
+    image = open_image(image_path)
+    if size:
+        image = resize_image(image, size=size)
     # Construct the data URL
-    return f"data:{mime_type};base64,{base64_encoded_data}"
+    return f"data:{mime_type};base64,{b64encode_image(image, format=format)}"
 
 
 def split_in_chunks(iterator, chunk_size) -> list[Iterator]:
