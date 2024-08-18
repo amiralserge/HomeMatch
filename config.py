995f9d04ab776c023e17fbb148fba071ca0036ea
amiralserge <kossononserge@yahoo.fr>
2024-08-17 22:30:48 -0400

    add test for test_local_image_to_data_url

--- config.py
+++ config.py
@@ -9,8 +9,21 @@ load_dotenv()
 
 @singleton
 class config(object):
+
     def __init__(self) -> None:
         for key, value in dotenv_values().items():
+            self.OPENAI_BASE_URL = https://api.openai.com/v1
+            self.OPENAI_API_KEY = None
+            self.MAX_TOKEN = 2000
+            self.LLM_MODEL = "gpt-4o-mini"
+            self.LLM_TEMPERATURE = 0
+            self.LLM_REQUEST_COOLDOWN_TIME = 5
+            self.LISTING_PICTURES_DIR = "./listing_pictures"
+            self.LISTING_PICTURES_DESCR_FILE = "./listing_pictures/pictures_descriptions.csv"
+            self.LISTING_FILE = "./picture_augmented_listings.csv"
+
+            self.VECTOR_DB_ENGINE = "lancedb"
+            self.VECTOR_DB_URI = "./homematch"
             setattr(self, key.lower(), value)
         # self._dotenv_values = dotenv_values()
 
--- utils/tests.py
+++ utils/tests.py
@@ -1,4 +1,8 @@
-from .utils import singleton, split_in_chunks
+from unittest import mock
+
+import pytest
+
+from .utils import local_image_to_data_url, singleton, split_in_chunks
 
 
 def test_singleton():
@@ -25,3 +29,17 @@ def test_split_in_chunks():
     assert split_in_chunks([1, 2], 3) == [[1, 2]]
     assert split_in_chunks([1, 2, 3], 5) == [[1, 2, 3]]
     assert split_in_chunks([1, 2, 3], 1) == [[1], [2], [3]]
+
+
+@mock.patch("utils.utils.guess_type")
+@mock.patch("utils.utils.b64encode_image")
+@mock.patch("utils.utils.open_image")
+def test_local_image_to_data_url(open_image_mock, encode_image_mock, guess_type_mock):
+    guess_type_mock.return_value = (None, None)
+    with pytest.raises(Exception, match="Could not detect mime type of file `test.img`"):
+        local_image_to_data_url("test.img")
+
+    guess_type_mock.return_value = ("image/jpeg", None)
+    encode_image_mock.return_value = "oTQjRXWoXj9Sw9+RZQSs+2mrNSIXRApUWgcTcx0iARWYl5gdYQyXzwdXra35Wd0AShs="
+    expected_result = "data:image/jpeg;base64,oTQjRXWoXj9Sw9+RZQSs+2mrNSIXRApUWgcTcx0iARWYl5gdYQyXzwdXra35Wd0AShs="
+    assert local_image_to_data_url("test.img") == expected_result
