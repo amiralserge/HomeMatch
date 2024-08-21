20d97c14ea9ca9a59599c363e5f7a881545b57bf
amiralserge <kossononserge@yahoo.fr>
2024-08-20 23:15:45 -0400

    improve listing model tests

--- models/tests.py
+++ models/tests.py
@@ -3,6 +3,10 @@ from models.listings import Listing, get_listing_summary
 
 class TestListingModel:
 
+    @classmethod
+    def _create_listing_record(cls, data):
+        return Listing(**data, listing_summary=get_listing_summary(data))
+
     def test_get_listing_description(self):
         listing_data = dict(
             price="650,000",
@@ -40,37 +44,38 @@ Neighborhood Description: Sunset Heights is a vibrant neighborhood known for its
         assert get_listing_summary(listing_data) == expected
 
     def test_listing_model(self):
-        data = dict(
-            price="$650,000",
-            bathrooms=2,
-            bedrooms=3,
-            house_size="1,800 sqft",
-            description=(
-                "Welcome to Sunset Heights, where modern living meets comfort! "
-                "This stunning 3-bedroom, 2-bathroom home features an inviting living "
-                "area adorned with a cozy sofa and a contemporary coffee table, "
-                "perfect for gatherings. The kitchen boasts high-end appliances "
-                "and a stylish island that provides both prep space and casual "
-                "dining. Enjoy the luxury of a backyard oasis complete with a patio for "
-                "entertaining and a beautifully landscaped garden. "
-                "Additional amenities include a two-car garage and access to a community gym, "
-                "ensuring a healthy lifestyle is at your fingertips."
-            ),
-            neighborhood="Sunset Heights",
-            neighborhood_description=(
-                "Sunset Heights is a vibrant neighborhood known for its family-friendly atmosphere and "
-                "a wealth of outdoor recreational options. Residents enjoy local parks, hiking trails, "
-                "and a community pool. The area offers convenient access to shopping, cafes, and schools, "
-                "making it an ideal place for families and professionals alike."
-            ),
+        listing_record1 = self._create_listing_record(
+            dict(
+                price="$650,000",
+                bathrooms=2,
+                bedrooms=3,
+                house_size="1,800 sqft",
+                description=(
+                    "Welcome to Sunset Heights, where modern living meets comfort! "
+                    "This stunning 3-bedroom, 2-bathroom home features an inviting living "
+                    "area adorned with a cozy sofa and a contemporary coffee table, "
+                    "perfect for gatherings. The kitchen boasts high-end appliances "
+                    "and a stylish island that provides both prep space and casual "
+                    "dining. Enjoy the luxury of a backyard oasis complete with a patio for "
+                    "entertaining and a beautifully landscaped garden. "
+                    "Additional amenities include a two-car garage and access to a community gym, "
+                    "ensuring a healthy lifestyle is at your fingertips."
+                ),
+                neighborhood="Sunset Heights",
+                neighborhood_description=(
+                    "Sunset Heights is a vibrant neighborhood known for its family-friendly atmosphere and "
+                    "a wealth of outdoor recreational options. Residents enjoy local parks, hiking trails, "
+                    "and a community pool. The area offers convenient access to shopping, cafes, and schools, "
+                    "making it an ideal place for families and professionals alike."
+                ),
+            )
         )
-        listing = Listing(**data, listing_summary=get_listing_summary(data))
 
-        assert listing.price == 650_000
-        assert listing.bathrooms == 2
-        assert listing.bedrooms == 3
-        assert listing.house_size == 1800
-        assert listing.description == (
+        assert listing_record1.price == 650_000
+        assert listing_record1.bathrooms == 2
+        assert listing_record1.bedrooms == 3
+        assert listing_record1.house_size == 1800
+        assert listing_record1.description == (
             "Welcome to Sunset Heights, where modern living meets comfort! "
             "This stunning 3-bedroom, 2-bathroom home features an inviting living "
             "area adorned with a cozy sofa and a contemporary coffee table, "
@@ -81,15 +86,16 @@ Neighborhood Description: Sunset Heights is a vibrant neighborhood known for its
             "Additional amenities include a two-car garage and access to a community gym, "
             "ensuring a healthy lifestyle is at your fingertips."
         )
-        assert listing.neighborhood == "Sunset Heights"
-        assert listing.neighborhood_description == (
+        assert listing_record1.neighborhood == "Sunset Heights"
+        assert listing_record1.neighborhood_description == (
             "Sunset Heights is a vibrant neighborhood known for its family-friendly atmosphere and "
             "a wealth of outdoor recreational options. Residents enjoy local parks, hiking trails, "
             "and a community pool. The area offers convenient access to shopping, cafes, and schools, "
             "making it an ideal place for families and professionals alike."
         )
 
-        expected_listing_summary = """
+        assert listing_record1.listing_summary == (
+            """
 Neighborhood: Sunset Heights
 Price: $650,000
 Bedrooms: 3
@@ -97,5 +103,33 @@ Bathrooms: 2
 House Size: 1,800 sqft
 Description: Welcome to Sunset Heights, where modern living meets comfort! This stunning 3-bedroom, 2-bathroom home features an inviting living area adorned with a cozy sofa and a contemporary coffee table, perfect for gatherings. The kitchen boasts high-end appliances and a stylish island that provides both prep space and casual dining. Enjoy the luxury of a backyard oasis complete with a patio for entertaining and a beautifully landscaped garden. Additional amenities include a two-car garage and access to a community gym, ensuring a healthy lifestyle is at your fingertips.
 Neighborhood Description: Sunset Heights is a vibrant neighborhood known for its family-friendly atmosphere and a wealth of outdoor recreational options. Residents enjoy local parks, hiking trails, and a community pool. The area offers convenient access to shopping, cafes, and schools, making it an ideal place for families and professionals alike.
-"""  # noqa: E501
-        assert listing.listing_summary == expected_listing_summary
+"""
+        )  # noqa: E501, E122
+
+        assert listing_record1.model_dump(
+            by_alias=True,
+            exclude=["id", "image", "image_vector", "listing_summary", "vector"],
+        ) == {
+            "Price": 650_000,
+            "Bathrooms": 2,
+            "Bedrooms": 3,
+            "House Size": 1_800,
+            "Description": (
+                "Welcome to Sunset Heights, where modern living meets comfort! "
+                "This stunning 3-bedroom, 2-bathroom home features an inviting living "
+                "area adorned with a cozy sofa and a contemporary coffee table, "
+                "perfect for gatherings. The kitchen boasts high-end appliances "
+                "and a stylish island that provides both prep space and casual "
+                "dining. Enjoy the luxury of a backyard oasis complete with a patio for "
+                "entertaining and a beautifully landscaped garden. "
+                "Additional amenities include a two-car garage and access to a community gym, "
+                "ensuring a healthy lifestyle is at your fingertips."
+            ),
+            "Neighborhood": "Sunset Heights",
+            "Neighborhood Description": (
+                "Sunset Heights is a vibrant neighborhood known for its family-friendly atmosphere and "
+                "a wealth of outdoor recreational options. Residents enjoy local parks, hiking trails, "
+                "and a community pool. The area offers convenient access to shopping, cafes, and schools, "
+                "making it an ideal place for families and professionals alike."
+            ),
+        }
